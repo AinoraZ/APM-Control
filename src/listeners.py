f406a6cfef9ccd20601319e817c54f4b9312263c
Ainoras <ainoraz@gmail.com>
2017-04-14 14:17:37 +0300

    Added more listeners (Battery voltage/level, compass, status(armed/disarmed), mode, air/ground speed)

--- src/listeners.py
+++ src/listeners.py
@@ -1,4 +1,5 @@
-import tools
+import config
+
 
 class Listen(object):
     def __init__(self, vehicle):
@@ -9,30 +10,51 @@ class Listen(object):
     def attitude_listener(self, attribute, name, msg):
         att = self.vehicle.attitude
         obj = {'pitch': att.pitch, 'yaw': att.yaw, 'roll': att.roll}
-        self.sio.emit('gyro', obj)
+        self.sio.emit('gyro_info', obj)
         #print obj
 
     def frame_listener(self, attribute, name, value):
-        glob = self.vehicle.location.global_relative_frame
-        obj = {'lat' : glob.lat, 'lng' : glob.lon, 'alt' : glob.alt}
-        self.sio.emit('location', obj)
-        #print obj
+        obj = {'lat': value.lat, 'lng': value.lon, 'alt': value.alt}
+        self.sio.emit('location_info', obj)
 
     def battery_listener(self, attribute, name, value):
-        voltage = self.vehicle.battery.voltage
-        #print voltage
-        self.sio.emit('battery', voltage)
+        max_dif = 4.2 * config.BATTERY_CELL_COUNT - 3 * config.BATTERY_CELL_COUNT
+        dif = value - (3 * config.BATTERY_CELL_COUNT)
+        level = int(dif/max_dif * 100)
+
+        obj = {'voltage': value, 'level': level}
+        print obj
+        self.sio.emit('battery_info', obj)
+
+    def compass_listener(self, attribute, name, value):
+            self.sio.emit('compass_info', value)
+
+    def arm_listener(self, attribute, name, value):
+        self.sio.emit('armed_info', value)
+
+    def mode_listener(self, attribute, name, value):
+        self.sio.emit('mode_info', value.name)
+
+    def speed_listener(self, attribute, name, value):
+        self.sio.emit('speed_info', {'groundspeed': self.vehicle.groundspeed, 'airspeed': value})
 
     def _add_listeners(self):
         self.vehicle.add_attribute_listener('attitude', self.attitude_listener)
         self.vehicle.add_attribute_listener('location.global_relative_frame', self.frame_listener)
-        self.vehicle.add_attribute_listener('battery', self.battery_listener)
+        self.vehicle.add_attribute_listener('battery.voltage', self.battery_listener)
+        self.vehicle.add_attribute_listener('heading', self.compass_listener)
+        self.vehicle.add_attribute_listener('armed', self.arm_listener)
+        self.vehicle.add_attribute_listener('mode', self.mode_listener)
+        self.vehicle.add_attribute_listener('airspeed', self.speed_listener)
 
     def _remove_listeners(self):
-        obj = {'lat' : 0, 'lng' : 0, 'alt' : 0}
-        self.sio.emit('location', obj)
         obj = {'pitch': 0, 'yaw': 0, 'roll': 0}
         self.sio.emit('gyro', obj)
+
         self.vehicle.remove_attribute_listener('attitude', self.attitude_listener)
         self.vehicle.remove_attribute_listener('location.global_relative_frame', self.frame_listener)
-        self.vehicle.remove_attribute_listener('battery', self.battery_listener)
+        self.vehicle.remove_attribute_listener('battery.voltage', self.battery_listener)
+        self.vehicle.remove_attribute_listener('heading', self.compass_listener)
+        self.vehicle.remove_attribute_listener('armed', self.arm_listener)
+        self.vehicle.remove_attribute_listener('mode', self.mode_listener)
+        self.vehicle.remove_attribute_listener('airspeed', self.speed_listener)
